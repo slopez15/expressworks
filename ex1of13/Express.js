bb53d768ece9ee61bb25f06e11c8e3251dbfd082
Saul Lopez <slopez15@asu.edu>
2017-02-05 00:48:46 -0700

    first commit. Adding ex1 work so far.

---
+++ ex1of13/Express.js
@@ -0,0 +1,31 @@
+/*Express.js
+remember to install express
+express app on port provided in console (process.argv[2])
+
+Vid: https://www.youtube.com/watch?v=C2IqQOLCCoU&list=PLguYmmjtxbWGwQRxXqMTQCj6FNb55aFVo
+//taskkill /IM node.exe
+    //kills all node tasks; IMPORTANT run this before verifying (I noticed other excersices have problems if you don not end a node)
+*/
+
+//define root modules //warned by jlint, that needed to be defined
+var console = require("console");
+var process = require("process");
+var require = require("require");
+
+console.log(process.argv[2]);
+//port provided by expressworks as first argv process.argv[2]
+//create express app on specified port
+/*var express = require('express');
+var app = express();
+app.get('/', function (req, res) {
+    'use strict'; //don't know why brackets provide this suggestion
+    res.end('Hello World!');
+});
+app.listen(process.argv[2]);
+*/
+
+
+
+
+
+
---
+++ ex1of13/zargv.js
@@ -0,0 +1,7 @@
+
+console.log("node full path: " + process.argv[0]);
+console.log("nano full path: " + process.argv[1]);
+
+console.log("input 1: " + process.argv[2]);
+console.log("input 2: " + process.argv[3]);
+console.log("input 3: " + process.argv[4]);
